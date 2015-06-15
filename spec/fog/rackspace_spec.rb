ac1fc9c4bb90746fad18ee8b02848b4d9efa7a93
Matt Darby <matt.darby@rackspace.com>
2015-06-15 15:24:11 -0400

    Moved over tests, etc

--- spec/fog/rackspace_spec.rb
+++ spec/fog/rackspace_spec.rb
@@ -9,3 +9,15 @@ describe Fog::Rackspace do
     expect(false).to eq(true)
   end
 end
+
+# From Fog proper
+# require "minitest/autorun"
+# require "fog"
+# require "fog/bin"
+# require "helpers/bin"
+#
+# describe Rackspace do
+#   include Fog::BinSpec
+#
+#   let(:subject) { Rackspace }
+# end
