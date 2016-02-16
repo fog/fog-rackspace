dda9454750d616a6fd65b4d1b3e9f99120eff82d
Matt Darby <matt.darby@rackspace.com>
2016-02-16 14:55:42 -0500

    Ported

---
+++ lib/fog/rackspace.rb
@@ -0,0 +1,25 @@
+require "fog/rackspace/version"
+
+module Fog
+  module Rackspace
+    # Your code goes here...
+  end
+end
+
+
+require 'fog/rackspace/auto_scale'
+require 'fog/rackspace/block_storage'
+require 'fog/rackspace/cdn'
+require 'fog/rackspace/cdn_v2'
+require 'fog/rackspace/compute'
+require 'fog/rackspace/compute_v2'
+require 'fog/rackspace/databases'
+require 'fog/rackspace/dns'
+require 'fog/rackspace/identity'
+require 'fog/rackspace/load_balancers'
+require 'fog/rackspace/monitoring'
+require 'fog/rackspace/queues'
+require 'fog/rackspace/storage'
+require 'fog/rackspace/networking'
+require 'fog/rackspace/orchestration'
+require 'fog/rackspace/networking_v2'
---
+++ spec/fog/rackspace_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Fog::Rackspace do
+  it 'has a version number' do
+    expect(Fog::Rackspace::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
